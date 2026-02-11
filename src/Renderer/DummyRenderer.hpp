9ed51ec043f43b50a63123013de40705d6c920d2
ARTurleite6 <arturleite164@gmail.com>
2026-02-11 18:13:06 +0000

    Receive Dummy Shader as parameter on DummyRenderer

--- src/Renderer/DummyRenderer.hpp
+++ src/Renderer/DummyRenderer.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
+#include "Camera/Camera.hpp"
 #include "Image/Image.hpp"
+#include "Shaders/Shader.hpp"
+#include "Utils/ProgressBar.hpp"
 
 namespace VI {
 class Scene;
@@ -8,6 +11,28 @@ class Camera;
 
 class DummyRenderer final {
 public:
-  Image Render(const Scene &scene, const Camera &camera);
+  template<Shader S>
+  Image Render(const Scene &scene, const Camera &camera, const S &shader) {
+    auto [width, height] = camera.GetResolution();
+
+    ProgressBar bar{static_cast<int>(width * height)};
+    Image image{static_cast<int>(width), static_cast<int>(height)};
+
+    for (int y = 0; y < height; ++y) {
+      for (int x = 0; x < width; ++x) {
+        Ray ray = camera.GenerateRay(x, y);
+        ray.Direction.x = x;
+        ray.Direction.y = y;
+
+        RGB color = shader.Execute(ray, scene);
+        image.Set(x, y, color);
+        bar.Increment();
+      }
+    }
+
+    bar.Finish();
+
+    return image;
+  }
 };
 } // namespace VI
